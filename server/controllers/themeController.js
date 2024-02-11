0e52109064e32ab2b85d8ab515a7433b1b406899
Marwa <km_zahzam@esi.dz>
2024-02-12 00:23:19 +0100

    finaaaaaaaaal

--- server/controllers/themeController.js
+++ server/controllers/themeController.js
@@ -1,31 +1,37 @@
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+
 const getThemes = async (req, res) => {
-  try {
-    const themes = await prisma.theme.findMany();
-    res.json(themes);
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({ error: e.message });
-  }
-};
+
+    try {
+        const themes = await prisma.theme.findMany()
+        res.json(themes);
+    }
+    catch (e) {
+        console.log(e);
+        res.status(500).json({ error: e.message });
+    }
+}
+
 
 const createTheme = async (req, res) => {
 
-    const { domId, designation, Duree, Niveau, TarifP, isCertif, devis, description, prerequis } = req.body;
+    const { domId, designation, Duree, NbFormateur, Niveau, TarifP, isCertif, devis, description, prerequis } = req.body;
     try {
         const theme = await prisma.theme.create({
             data: {
                 domId : domId,
                 designation : designation,
                 Duree : Duree,      
+                NbFormateur: NbFormateur,
                 Niveau :  Niveau,     
                 TarifP : TarifP,   
                 isCertif: isCertif,
                 devis: devis,
                 description : description,
                 prerequis : prerequis 
+
             }
         }).then((theme) => {
             res.json(theme);
@@ -41,62 +47,76 @@ const createTheme = async (req, res) => {
 
 
 const deleteTheme = async (req, res) => {
-  const { id } = req.params;
 
-  try {
-    const theme = await prisma.theme.delete({
-      where: {
-        id: parseInt(id),
-      },
-    });
-    if (!theme) {
-      return res.status(404).json({ error: "theme not found" });
+    const {id} = req.params;
+
+    try {
+
+        const theme = await prisma.theme.delete({
+            where: {
+                id: parseInt(id)
+            }
+        })
+        if (!theme) {
+            return res.status(404).json({error: 'theme not found'})
+        }
+        res.status(200).json({message: 'theme deleted'})
+        
+        
     }
-    res.status(200).json({ message: "theme deleted" });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
+    catch (error) {
+        res.status(400).json({error: error.message})
+    }
+
+}
+
 
 const getThemeByDomaine = async (req, res) => {
-  const { domId } = req.params;
-  try {
-    const themes = await prisma.theme.findMany({
-      where: {
-        domId: parseInt(domId),
-      },
-    });
-    if (!themes) {
-      return res.status(404).json({ error: "themes not found" });
+
+    const {domId} = req.params;
+    try {
+        const themes = await prisma.theme.findMany({
+            where: {
+                domId: parseInt(domId)
+            }
+        })
+        if (!themes) {
+            return res.status(404).json({error: 'themes not found'})
+        }
+        res.json(themes);
     }
-    res.json(themes);
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({ error: e.message });
-  }
-};
+    catch (e) {
+        console.log(e);
+        res.status(500).json({ error: e.message });
+    }
+}
 
 const getThemeByDomaine2 = async (domId) => {
-  try {
-    const themes = await prisma.theme.findMany({
-      where: {
-        domId: parseInt(domId),
-      },
-    });
-    if (!themes) {
-      return null;
+
+    
+    try {
+        const themes = await prisma.theme.findMany({
+            where: {
+                domId: parseInt(domId)
+            }
+        })
+        if (!themes) {
+            return null;
+        }
+        return themes;
     }
-    return themes;
-  } catch (e) {
-    console.log(e);
-    return null;
-  }
-};
+    catch (e) {
+        console.log(e);
+        return null;
+    }
+}
+
+    
 
 module.exports = {
-  getThemes,
-  createTheme,
-  deleteTheme,
-  getThemeByDomaine,
-  getThemeByDomaine2,
-};
+    getThemes,
+    createTheme,
+    deleteTheme,
+    getThemeByDomaine,
+    getThemeByDomaine2
+}
