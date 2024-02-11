3d57e8b1c8ce526ea7b34681fb590516bc0cc22a
NorhaneCh <kn_cherif@esi.dz>
2024-02-11 23:40:18 +0100

    last norhane

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import { cooperation ,logo} from '../assets/index.js';
-import { Link, useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import { cooperation, logo } from "../assets/index.js";
+// import {useHistory} from "react-router-dom";
 
 const SignIn = () => {
-  const history = useHistory(); 
+  // const history = useHistory();
   const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-    role: 'formateur', 
+    email: "",
+    password: "",
+    role: "formateur",
   });
 
   const { email, password, role } = formData;
@@ -20,34 +20,43 @@ const SignIn = () => {
     e.preventDefault();
 
     //HNA ON VERIFIE
-    const isAuthenticated = true; 
+    const isAuthenticated = true;
 
     if (isAuthenticated) {
-      console.log('Form Data:', formData);
-      history.push('/catalogue'); 
+      console.log("Form Data:", formData);
+      history.push("/catalogue");
     }
   };
 
   return (
     <div className="flex h-screen">
-   
-       <div className="bg-color-blue flex-shrink-0 w-1/2 flex items-center justify-center">
-        <img src={cooperation} alt="frame" className="w-[700px] h-[700px] object-cover" />
+      <div className="bg-color-blue flex-shrink-0 w-1/2 flex items-center justify-center">
+        <img
+          src={cooperation}
+          alt="frame"
+          className="w-[700px] h-[700px] object-cover"
+        />
+      </div>
+
+      <div
+        className="flex-shrink-0 w-1/2 bg-white p-8 rounded shadow-md flex flex-col gap-8 items-center justify-center "
+        style={{ marginTop: "-70px" }}
+      >
+        <div className="flex flex-col  items-center justify-center mt-1 ">
+          <h2 className="font-semibold text-[22px]">Bienvennu dans</h2>
+          <img src={logo} alt="frame" className="w-[270px] h-[120px] " />
         </div>
 
-     
-      <div className="flex-shrink-0 w-1/2 bg-white p-8 rounded shadow-md flex flex-col gap-8 items-center justify-center " style={{ marginTop: '-70px'}}>
-      
-      <div className='flex flex-col  items-center justify-center mt-1 '>
-          <h2 className='font-semibold text-[22px]'>Bienvennu dans</h2>
-          <img src={logo} alt="frame" className="w-[270px] h-[120px] "/>
-
-        </div>
-      
-        
-        <form className="w-full max-w-md" onSubmit={handleSubmit} style={{ marginTop: '20px'}}>
+        <form
+          className="w-full max-w-md"
+          onSubmit={handleSubmit}
+          style={{ marginTop: "20px" }}
+        >
           <div className="mb-8">
-            <label htmlFor="email" className="block text-gray-700 text-base font-medium mb-2">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 text-base font-medium mb-2"
+            >
               Email
             </label>
             <input
@@ -62,7 +71,10 @@ const SignIn = () => {
             />
           </div>
           <div className="mb-8">
-            <label htmlFor="password" className="block text-gray-700 text-base font-medium mb-2">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 text-base font-medium mb-2"
+            >
               Password
             </label>
             <input
@@ -77,7 +89,10 @@ const SignIn = () => {
             />
           </div>
           <div className="mb-8">
-            <label htmlFor="role" className="block text-gray-700 text-base font-medium mb-2">
+            <label
+              htmlFor="role"
+              className="block text-gray-700 text-base font-medium mb-2"
+            >
               Vous êtes un:
             </label>
             <select
